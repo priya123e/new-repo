f2de99b92939ec5cffbb3b5ada1da11fcaed50a3
priya123e <111388531+priya123e@users.noreply.github.com>
2023-02-11 02:09:30 -0500

    added print in routes.py

--- routes.py
+++ routes.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/login", methods=['GET'])
 def hello():
+    print("hello")
     return render_template('login.html')
 if __name__ == "__main__":
     app.run(debug=True)
